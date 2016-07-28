350a073bbc3e9387e2a4025cf138fcfe96206eb3
Andrew <andrew@getprivy.com>
2016-07-28 12:33:20 -0400

    Added puma config

---
+++ config/puma.rb
@@ -0,0 +1,14 @@
+workers Integer(ENV["WEB_CONCURRENCY"] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+port    ENV['PORT'] || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
